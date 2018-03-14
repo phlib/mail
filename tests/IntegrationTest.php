fbe95c8301cbdb4d2ff6f6eceb5cea9e171ab443
Martin Price <martin.price@emailcenteruk.com>
2018-03-14 14:01:57 +0000

    Create integration tests for testing using the outputted mail string as input to the mail factory
    This includes a breaking test which highlights an issue with having no trailing new lines in the content part of a content only mail string. Due to a bug in mailparse (https://bugs.php.net/bug.php?id=75923), the last line of the mail string is discarded if it does not end with a trailing new line.

---
+++ tests/IntegrationTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Phlib\Mail\Tests;
+
+use Phlib\Mail\AbstractPart;
+use Phlib\Mail\Content\AbstractContent;
+use Phlib\Mail\Content\Html;
+use Phlib\Mail\Content\Text;
+use Phlib\Mail\Factory;
+use Phlib\Mail\Mime\AbstractMime;
+use Phlib\Mail\Mime\MultipartAlternative;
+
+class IntegrationTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testRecreateMultipartMail()
+    {
+        $originalMail = new \Phlib\Mail\Mail();
+        $originalMail->addTo('to@example.com');
+        $originalMail->setFrom('from@example.com');
+        $originalMail->setSubject('Subject line');
+        $contentPart = new MultipartAlternative();
+        $contentPart->addPart((new Html())
+            ->setContent('Test <b>html</b>')
+            ->setEncoding(AbstractPart::ENCODING_BASE64));
+        $contentPart->addPart((new Text())
+            ->setContent('Test text')
+            ->setEncoding(AbstractPart::ENCODING_BASE64));
+        $originalMail->setPart($contentPart);
+
+        $source = $originalMail->toString();
+
+        $mail = (new Factory())->createFromString($source);
+
+        /** @var AbstractMime $mainPart */
+        $mainPart = $mail->getPart();
+        $this->assertInstanceOf(AbstractMime::class, $mainPart);
+        $parts    = $mainPart->getParts();
+        $this->assertCount(2, $parts);
+        /** @var AbstractContent $htmlPart */
+        /** @var AbstractContent $textPart */
+        list($htmlPart, $textPart) = $parts;
+
+        $this->assertEquals('Test <b>html</b>', $htmlPart->getContent());
+        $this->assertEquals('Test text', $textPart->getContent());
+    }
+
+    public function testRecreateContentOnlyMail()
+    {
+        $originalMail = new \Phlib\Mail\Mail();
+        $originalMail->addTo('to@example.com');
+        $originalMail->setFrom('from@example.com');
+        $originalMail->setSubject('Subject line');
+        $contentPart = (new Text())
+            ->setContent('Test text')
+            ->setEncoding(AbstractPart::ENCODING_BASE64);
+        $originalMail->setPart($contentPart);
+
+        $source = $originalMail->toString();
+
+        $mail = (new Factory())->createFromString($source);
+
+        /** @var AbstractContent $mainPart */
+        $mainPart = $mail->getPart();
+        $this->assertInstanceOf(AbstractContent::class, $mainPart);
+
+        $this->assertEquals('Test text', $mainPart->getContent());
+    }
+}
